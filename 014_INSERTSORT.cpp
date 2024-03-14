0ee86d59f4a80d0e5672cc9b091e9757c3cec49f
ahsanhf <ahsanhf@gmail.com>
2024-03-14 17:40:45 +0700

    memasukkan input

--- 014_INSERTSORT.cpp
+++ 014_INSERTSORT.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
+int arr[9];
+int n;
+
+void input() {         //prosedur untuk input
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";   //output ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) //jika n kurang dari atau sama dengan 20
+			break; //keluar dari loop
+		else {     //jika n lebih dari 20
+			cout << "\narray dapat mempunyai maksimal 20 elemen.\n"; //output ke layar
+		}
+	}
+	cout << endl;                          //output baris kosong
+	cout << "===================" << endl; //output ke layar
+	cout << "masukan elemen array" << endl;//output ke layar
+	cout << "===================" << endl; //output ke layar
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
 
 void insertionSort() {
     int i, n, temp, j;
@@ -16,6 +39,7 @@ void insertionSort() {
         arr[j + 1] = temp; //step 5
     }
 }
+
 int main()
 {
    
